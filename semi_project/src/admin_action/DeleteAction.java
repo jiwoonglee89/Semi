1c9bd6e79e601c57d52e4534da6060dfb515e040
leewo <leewo@Desktop-lee>
2016-04-26 00:18:26 +0900

    수정

---
+++ semi_project/src/admin_action/DeleteAction.java
@@ -0,0 +1,30 @@
+package admin_action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.ibatis.session.SqlSession;
+
+import action.*;
+
+public class DeleteAction implements CommandAction{
+
+	@Override
+	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		Connection con=new Connection();
+		SqlSession session=con.connection();
+		
+		String p_id=request.getParameter("id");
+		int success=session.insert("admin.remove_per", p_id);
+		if(success>0){
+			session.commit();
+			
+		}else
+		{
+			session.rollback();
+		}
+		return "admin_main.jsp";
+		
+	}
+
+}
--- semi_project/src/admin_action/MainAction.java
+++ semi_project/src/admin_action/MainAction.java
@@ -3,10 +3,9 @@ package admin_action;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+import action.CommandAction;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.apache.ibatis.session.SqlSession;
 
 public class MainAction implements CommandAction{
@@ -15,11 +14,17 @@ public class MainAction implements CommandAction{
 	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
 		Connection con=new Connection();
 		SqlSession session=con.connection();
+		int num=Integer.parseInt(request.getParameter("who"));
+		request.setAttribute("who", num);
+		
 		List people=session.selectList("admin.all_per");
-		List corporation=session.selectList("admin.all_cor");
+		
 		request.setAttribute("people", people);
-		request.setAttribute("corporation", corporation);
-		return "admin_main.jsp";
+
+		//List corporation=session.selectList("admin.all_cor");
+		request.setAttribute("people", people);
+		//request.setAttribute("corporation", corporation);
+		return "/admin_main.jsp";
 	}
 
 }
