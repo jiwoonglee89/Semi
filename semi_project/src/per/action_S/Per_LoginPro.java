7a56cf8f8b4be22151c816c3606a4b36c1b28075
wonmo <lym09080@naver.com>
2016-04-25 15:54:50 +0900

    wonmo

--- semi_project/src/per/action_S/Per_LoginPro.java
+++ semi_project/src/per/action_S/Per_LoginPro.java
@@ -2,6 +2,7 @@ package per.action_S;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.ibatis.session.SqlSession;
 
