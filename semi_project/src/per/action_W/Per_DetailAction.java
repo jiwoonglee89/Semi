c28bf84903090817c20a39c6476baabbf6333ede
wonmo <lym09080@naver.com>
2016-04-26 21:19:15 +0900

    w

--- semi_project/src/per/action_W/Per_DetailAction.java
+++ semi_project/src/per/action_W/Per_DetailAction.java
@@ -8,37 +8,29 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.ibatis.session.SqlSession;
 
-import DB.P_MemBean;
+import DB.FileBean;
 import action.CommandAction;
-import action.Connection;
 
-public class Per_MainViewAction implements CommandAction{
+public class Per_DetailAction implements CommandAction {
 
 	@Override
 	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
 		// TODO Auto-generated method stub
 		
-		request.setCharacterEncoding("UTF-8");
+		Connection con = new Connection();
+		SqlSession session = con.connection();
 		
-		Connection con=new Connection();
-		SqlSession session=con.connection();
-		
-		
-		List fileList = null;
 		
 		HttpSession session2 = request.getSession();
 		String p_id = (String) session2.getAttribute("p_id");
 		
-		//P_MemBean pbean = session.selectOne("per_member.find",p_id);
-			
+		List fileList = session.selectList("file.all",p_id);
 		
-		fileList = session.selectList("file.all",p_id );
-		P_MemBean p_list = session.selectOne("per_member.find",p_id);
+		//FileBean fbean = (FileBean) session.selectList("file.all",p_id);
 		
 		request.setAttribute("fileList", fileList);
-		request.setAttribute("p_list",p_list);
-				
-		return "p_mainview.jsp";
+		//request.setAttribute("fbean", fbean);
+		return "p_detail.jsp";
 	}
 
 }
