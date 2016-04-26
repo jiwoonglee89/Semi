b54fd6b626a3b9bdca87a08479887e48f2213f10
leewo <leewo@Desktop-lee>
2016-04-26 17:35:39 +0900

    수정

--- semi_project/src/action/Co_mainAction.java
+++ semi_project/src/action/Co_mainAction.java
@@ -3,11 +3,16 @@ package action;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.ibatis.session.SqlSession;
+
 public class Co_mainAction implements CommandAction{
 
 	@Override
 	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
-		// TODO Auto-generated method stub
+		
+		Connection con=new Connection();
+		SqlSession session=con.connection();
+		
 		return "co_main.jsp";
 	}
 
